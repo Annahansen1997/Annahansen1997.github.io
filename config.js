90c74b37847c0ec86e743a10a52609509e39dd64
Anna Hansen <annahans@hotmail.com>
2025-02-02 21:19:38 +0100

    Payment link stripe

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Stripe konfigurasjon - Nøkler er flyttet til .env fil
 const config = {
-    PAYMENT_LINK: 'https://buy.stripe.com/4gw6qj0UQb8K6647ss'
+    PAYMENT_LINK: 'https://buy.stripe.com/bIYcOHbzuccO0LKcMN'
 }; 
