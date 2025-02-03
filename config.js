019657a0990ef46628a02c1b8c22e5ce3600feab
Anna Hansen <annahans@hotmail.com>
2025-02-03 10:10:39 +0100

    Fjernet payment lenke

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Stripe konfigurasjon - Felles betalingslenke for alle produkter
 const config = {
-    PAYMENT_LINK: 'https://buy.stripe.com/bIYcOHbzuccO0LKcMN'
+    PAYMENT_LINK: 'https://buy.stripe.com/bIYcOHbzuccO0LKcMN',
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
 }; 
