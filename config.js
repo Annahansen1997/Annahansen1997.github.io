b4c973e5698db9743820e00105d09daa5bb14bbe
Anna Hansen <annahans@hotmail.com>
2025-02-03 10:13:54 +0100

    Publis key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Stripe konfigurasjon - Felles betalingslenke for alle produkter
 const config = {
     PAYMENT_LINK: 'https://buy.stripe.com/bIYcOHbzuccO0LKcMN',
-    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
+    STRIPE_PUBLISHABLE_KEY: 'pk_live_51Qmu3ULPxmfy63yEbYUAv6FZFaaGsoSTp8XF7nUEol9ksHgNid71K4FogSAhBwBDdNYa8syBZ4DAP4c9BS0qHaBQ00aT9p4bcV'
 }; 
