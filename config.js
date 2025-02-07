449fb4cfaf5f21eb04140763c15451ba44ae2da7
Anna Hansen <annahans@hotmail.com>
2025-02-07 10:49:24 +0100

    07.02 kl 10.50, betalingskodene i stripe

--- config.js
+++ config.js
@@ -2,4 +2,15 @@
 const config = {
     PAYMENT_LINK: 'https://buy.stripe.com/bIYcOHbzuccO0LKcMN',
     STRIPE_PUBLISHABLE_KEY: 'pk_live_51Qmu3ULPxmfy63yEbYUAv6FZFaaGsoSTp8XF7nUEol9ksHgNid71K4FogSAhBwBDdNYa8syBZ4DAP4c9BS0qHaBQ00aT9p4bcV'
-}; 
+};
+
+const PRODUCT_PRICES = {
+    "Flybingo": "price_1Qo9PnLPxmfy63yEf9cE5DIr",
+    "Bilbingo": "price_1Qo9P1LPxmfy63yES6FrJHo3",
+    "Enhjørningens magiske eventyrhefte": "price_1Qo9ODLPxmfy63yEtbAchGtn",
+    "På eventyr med dinosaurene": "price_1Qo9NKLPxmfy63yEAoCoz18f",
+    "Påskekos Aktivitetshefte": "price_1Qo9MJLPxmfy63yETbGYTyLJ",
+    "Vinterkos Aktivitetshefte": "price_1Qo9IPLPxmfy63yEXy1w1l8T"
+};
+
+export { config, PRODUCT_PRICES };
